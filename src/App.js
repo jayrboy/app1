63eb3b1c2ebd6a01c1392a77e5672ca46018b928
jayrboy <j.jayox_@hotmail.com>
2023-12-25 11:26:20 +0700

    Update App.js css inline
    Update App.js css inline

--- src/App.js
+++ src/App.js
@@ -6,10 +6,39 @@ function App() {
   const el = (
     <div>
       Create Web & Mobile Apps <br />
-      with <span dangerouslySetInnerHTML={{__html: r}}/> & {rn}
+      with <span dangerouslySetInnerHTML={{ __html: r }} /> & {rn}
     </div>
   )
-  return el;
+
+  const t = <table border="1">
+    <tr>
+      <th>Product</th>
+      <th>Price</th>
+    </tr>
+    <tr>
+      <td>React</td>
+      <td>320</td>
+    </tr>
+    <tr>
+      <td>React Native</td>
+      <td>340</td>
+    </tr>
+  </table>
+
+  const divStyle = {
+    color: 'red',
+    backgroundColor: 'powderblue',
+    fontSize: 'larger',
+    padding: '10px',
+  }
+
+  return (
+    <>
+      <div style={divStyle}>Hello React</div> < br />
+      {el} < br />
+      {t}
+    </>
+  );
 }
 
 export default App;
